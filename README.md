ed3a0b2847c94bd9d952986b0b8afc6a3ab97f97
兰学兴 <lanxuexing@xdf.cn>
2019-01-23 10:47:09 +0800

    init project

---
+++ README.md
@@ -0,0 +1,50 @@
+## vue-toast 
+
+-----------------------
+
+### Install
+
+`npm install @lanxuexing/vue-toast --save`
+
+
+### Settings
+
+Name                | Default                  |  Type    | Description
+--------------------|--------------------------|----------|---------------------------
+duration            | 200                      | Number   | The duration of the toast
+callback            | null                     | Number   | The callback of the toast
+
+### Usage
+
+There are two different ways to manage a Toast.
+
+##### **Calling api**
+
+```
+// import plugin
+import VueToast from 'vue-toast';
+
+// register plugin on vue
+Vue.use(VueToast)
+
+// add a toast on screen
+this.$toast.show("Hello, Toast");
+
+// you can manually hide the toast, or it will automatically disappear after a `duration` ms timeout.
+this.$toast.show("Hello, Toast", {
+  duration: 500
+});
+
+// you can add callback functions after toast display is complete
+this.$toast.show("Hello, Toast", function() {
+    // do anything
+});
+
+```
+
+### Run example:
+
+```
+cd ./vue-toast
+npm install
+```
---
+++ src/lib/vue-toast.vue
@@ -0,0 +1,82 @@
+<template>
+  <section class="toast-container">
+    <div class="toast" v-bind:class="[visible ? 'fade-in' : 'fade-out']">
+      <span class="message">{{message}}</span>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      visible: false,
+      message: ""
+    };
+  }
+};
+</script>
+
+<style lang='scss'>
+$fontSize: 16px !default;
+@function pxToRem($px) {
+  @return $px / $fontSize * 1rem;
+}
+@function pxToEm($px) {
+  @return $px / $fontSize * 1em;
+}
+.toast-container {
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  z-index: 1999;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .toast {
+    width: pxToRem(180px);
+    height: pxToRem(60px);
+    line-height: pxToRem(60px);
+    text-align: center;
+    background-color: rgba(0, 0, 0, 0.61);
+    border-radius: 10px;
+    color: white;
+  }
+  .fade-in {
+    animation-name: fade-in;
+    animation-duration: 0.3s;
+    animation-fill-mode: both;
+  }
+  .fade-out {
+    animation-name: fade-out;
+    animation-duration: 0.3s;
+    animation-fill-mode: both;
+  }
+  .message {
+    font-size: pxToRem(15px);
+    color: #fff;
+  }
+}
+@keyframes fade-in {
+  0% {
+    opacity: 0;
+    transform: scale(0.7);
+  }
+  100% {
+    opacity: 1;
+    transform: scale(1);
+  }
+}
+@keyframes fade-out {
+  0% {
+    opacity: 1;
+    transform: scale(1);
+  }
+  100% {
+    opacity: 0;
+    transform: scale(0.7);
+  }
+}
+</style>
